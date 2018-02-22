1e84f3d1e6987738a58cef9d90f34a98585e1de4
Nathan Carter Williams <nathancarterwilliams@gmail.com>
2018-02-22 16:35:12 -0500

    Autofil tiles flash

---
+++ Assets/flashScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashScript : MonoBehaviour {
+	[SerializeField]
+	Color c1;
+	[SerializeField]
+	Color c2;
+	[SerializeField]
+	float period;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<SpriteRenderer>().color = (Time.time % period < (period / 2) ? c1 : c2);
+	}
+}
