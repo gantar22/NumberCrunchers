10da0ab3d504270296e3f3f567061abba40b59c1
jinheel1 <jinheel1@andrew.cmu.edu>
2018-02-22 21:19:42 -0500

    made bgm script and added background music with base and looped versions

---
+++ Assets/code/BackgroundMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	public AudioClip musicLoop;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audioSource.isPlaying) {
+			GetComponent<AudioSource> ().loop = true;
+			audioSource.clip = musicLoop;
+			audioSource.Play ();
+		}
+	}
+}
